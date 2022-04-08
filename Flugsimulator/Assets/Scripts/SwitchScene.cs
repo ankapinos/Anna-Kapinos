c35c87953a480f3df3c3520159d70a3c37aa0081
ankapinos <102355363+ankapinos@users.noreply.github.com>
2022-04-08 17:30:43 +0200

    Flugsimulator

---
+++ Flugsimulator/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement ;
+
+public class SwitchScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(this.transform.rotation.z);
+        if (this.transform.rotation.z>=0.6){
+                 SceneManager.LoadScene(sceneName:"Cockpit");
+
+        }
+    }
+}
